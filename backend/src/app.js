a1a5711850d35614b780113f6d74836ef1983e67
Faza Lukman <fazamanul@gmail.com>
2026-05-21 22:26:37 +0700

    add health route

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
-console.log("APP START");
-
 const app = express();
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Backend healthy",
+  });
+});
 
 const prisma = require("./config/db");
 
